a185bd5703896a46dbf159d7c61b26bf1d9dd880
xujin <Softwar_King@qq.com>
2019-02-16 22:40:32 +0800

    升级测试

---
+++ ch18-8/ch18-8-gateway/src/main/java/cn/springcloud/book/gateway/repository/UnifiedRouteRepositoryImpl.java
@@ -0,0 +1,38 @@
+package cn.springcloud.book.gateway.repository;
+
+
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.Collections.synchronizedMap;
+
+public class UnifiedRouteRepositoryImpl implements  RouteDefinitionRepository {
+
+    private final Map<String, RouteDefinition> routes = synchronizedMap(new LinkedHashMap<String, RouteDefinition>());
+
+
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+        return null;
+    }
+
+
+
+
+
+}
